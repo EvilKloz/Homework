6ff04fe7dca743b389e6efa4ddfbb8cbb1ed57d5
KIRILL ZLOKAZOV <100695130+EvilKloz@users.noreply.github.com>
2022-03-25 23:07:32 +0300

    Add files via upload

---
+++ Fibonacci.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Fibonacci_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("До какого числа считать ряд Фибоначчи?");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int pervoe = 1;
+            Console.Write("{0} ", pervoe);
+            int vtoroe = 1;
+            Console.Write("{0} ", vtoroe);
+            int sum = 0;
+
+            while (number >= sum)
+            {
+                sum = pervoe + vtoroe;
+
+                Console.Write("{0} ", sum);
+
+                pervoe = vtoroe;
+                vtoroe = sum;
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
