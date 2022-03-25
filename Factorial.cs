00387d2ba0a4e574abd5790d6aad225c4412e987
KIRILL ZLOKAZOV <100695130+EvilKloz@users.noreply.github.com>
2022-03-25 23:45:09 +0300

    Add files via upload

---
+++ Factorial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Factorial_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число для вычисления факториала:");
+            long n = Convert.ToInt64(Console.ReadLine());
+            long r = 1;
+            for (int i = 2; i <= n; ++i)
+                r *= i;
+            Console.WriteLine("Результат: " + r);
+            Console.ReadLine();
+        }
+    }
+}
